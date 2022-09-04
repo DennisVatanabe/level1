26996d3b1cf83d3b9a5d12b6907c2b7e7018c6ad
DennisVatanabe <4dennis4parmon@gmail.com>
2022-09-04 05:42:17 +0300

    0409 _adaptive

--- script.js
+++ script.js
@@ -1,12 +1,31 @@
 const swiper = new Swiper('.swiper', {
-    // Optional parameters
+
     direction: 'horizontal',
     loop: true,
 
-    // If we need pagination
     pagination: {
     el: '.swiper-pagination', 
     clickable: true,
-    // dynamicBullets: true,
     },
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelector(".banner__burger").addEventListener("click", function() {
+        document.querySelector(".banner__header-nav").style.display = "block"
+        document.querySelector(".banner__cross").style.display ="block"
+        document.querySelector(".banner__burger").style.display = "none"
+        document.querySelector(".banner__title").style.display = "none"
+        document.querySelector(".banner__subtitle").style.display = "none"
+        document.querySelector(".banner__button").style.display = "none"
+    })
+
+    document.querySelector(".banner__cross").addEventListener("click", function() {
+        document.querySelector(".banner__header-nav").style.display = "none"
+        document.querySelector(".banner__cross").style.display = "none"
+        document.querySelector(".banner__burger").style.display ="block"
+        document.querySelector(".banner__header-logo").style.display = "inherit"
+        document.querySelector(".banner__title").style.display = "block"
+        document.querySelector(".banner__subtitle").style.display = "block"
+        document.querySelector(".banner__button").style.display = "block"
+    })
+})
